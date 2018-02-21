aed24c23caf2fe61c617e3c109f448c6753c8aa5
Fadhil Ikhsan <f.ikhsanta@gmail.com>
2018-02-21 13:23:58 +0700

    Initial

---
+++ app/src/main/java/ikhsan/fadhil/project/android/mycollege/Senin.java
@@ -0,0 +1,13 @@
+package ikhsan.fadhil.project.android.mycollege;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Senin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senin);
+    }
+}
